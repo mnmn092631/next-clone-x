61e08f10560973cbc4e7fd9a009ed951091e2efa
mnmn092631 <mnmn092631@gmail.com>
2024-04-23 00:34:57 +0900

    profile 페이지

--- src/app/(afterLogin)/[username]/page.tsx
+++ src/app/(afterLogin)/[username]/page.tsx
@@ -1,1 +1,52 @@
-export default function Page(){}
+import style from "./profile.module.css";
+import Post from "@/app/(afterLogin)/_component/Post";
+
+export default function Page() {
+  const user = {
+    id: "mnmn092631",
+    nickname: "mnmn",
+    image: "/laura-barry-K8su4Z7ah2w-unsplash.jpg",
+  };
+
+  return (
+    <main className={style.main}>
+      <div className={style.header}>
+        <button className={style.backButton}>
+          <svg
+            width={24}
+            viewBox="0 0 24 24"
+            aria-hidden="true"
+            className="r-18jsvk2 r-4qtqp9 r-yyyyoo r-z80fyv r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-19wmn03"
+          >
+            <g>
+              <path d="M7.414 13l5.043 5.04-1.414 1.42L3.586 12l7.457-7.46 1.414 1.42L7.414 11H21v2H7.414z"></path>
+            </g>
+          </svg>
+        </button>
+        <h3 className={style.headerTitle}>{user.nickname}</h3>
+      </div>
+      <div className={style.userZone}>
+        <div className={style.userImage}>
+          <img src={user.image} alt={user.id} />
+        </div>
+        <div className={style.userName}>
+          <div>{user.nickname}</div>
+          <div>@{user.id}</div>
+        </div>
+        <button className={style.followButton}>팔로우</button>
+      </div>
+      <div>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </main>
+  );
+}
