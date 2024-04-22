acb8c68d9fb6035c6bcf300899fea0819fc43eea
mnmn092631 <mnmn092631@gmail.com>
2024-04-23 01:25:12 +0900

    게시물 상세 페이지

--- src/app/(afterLogin)/[username]/status/[id]/page.tsx
+++ src/app/(afterLogin)/[username]/status/[id]/page.tsx
@@ -1,1 +1,24 @@
-export default function Page(){}
+import style from "./singlePost.module.css";
+import BackButton from "@/app/(afterLogin)/_component/BackButton";
+import Post from "@/app/(afterLogin)/_component/Post";
+import CommentForm from "@/app/(afterLogin)/[username]/status/[id]/_component/CommentForm";
+
+export default function Page() {
+  return (
+    <main className={style.main}>
+      <div className={style.header}>
+        <BackButton />
+        <h3 className={style.headerTitle}>게시하기</h3>
+      </div>
+      <Post />
+      <CommentForm />
+      <div>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </main>
+  );
+}
