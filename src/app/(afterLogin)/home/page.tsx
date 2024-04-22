86b59e71022865e6529263fab4a21b8cf21b6c3a
mnmn092631 <mnmn092631@gmail.com>
2024-04-23 00:20:48 +0900

    게시하기 모달

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -4,7 +4,7 @@ import Post from "@/app/(afterLogin)/_component/Post";
 import TabProvider from "@/app/(afterLogin)/home/_component/TabProvider";
 import style from "./home.module.css";
 
-export default function Page() {
+export default function Home() {
   return (
     <main className={style.main}>
       <TabProvider>
