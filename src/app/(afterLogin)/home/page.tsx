4a08185a868e574c0aff16e89fe6129a580b4db3
mnmn092631 <mnmn092631@gmail.com>
2024-04-22 20:50:03 +0900

    home 화면 구현

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,1 +1,31 @@
-export default function Page(){}
+import Tab from "@/app/(afterLogin)/home/_component/Tab";
+import PostForm from "@/app/(afterLogin)/home/_component/PostForm";
+import Post from "@/app/(afterLogin)/_component/Post";
+import TabProvider from "@/app/(afterLogin)/home/_component/TabProvider";
+import style from "./home.module.css";
+
+export default function Page() {
+  return (
+    <main className={style.main}>
+      <TabProvider>
+        <Tab />
+        <PostForm />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </TabProvider>
+    </main>
+  );
+}
